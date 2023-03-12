079a67bf1e05d418d3e09533a20645eb7d98ac70
sparc flow <sparc.flow@protonmail.com>
2023-03-12 19:19:10 +0100

    add serverless definition and ssm secret

--- cloudtrail_to_elastic/main.go
+++ cloudtrail_to_elastic/main.go
@@ -8,17 +8,17 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
-const (
-	elasticHost = "http://192.168.1.38:9200"
-	indexPrefix = "cloudtrail"
+var (
+	elasticHost    = os.Getenv("ELASTIC_HOST")
+	indexPrefix    = os.Getenv("cloudtrail")
+	elasticHeaders = map[string]string{
+		"Content-Type":  "application/json",
+		"Authorization": fmt.Sprintf("ApiKey %s", getSecret(os.Getenv("ES_API_KEY"))),
+	}
 )
-
-var elasticHeaders = map[string]string{
-	"Content-Type":  "application/json",
-	"Authorization": fmt.Sprintf("ApiKey %s", os.Getenv("ES_API_KEY")),
-}
 
 func main() {
 	sess := session.Must(session.NewSession())
@@ -27,4 +27,18 @@ func main() {
 		S3Client: s3.New(sess, &aws.Config{}),
 	}
 	lambda.Start(handler.ProcessRequest)
+}
+
+func getSecret(key string) string {
+	sess := session.Must(session.NewSession())
+	ssmClient := ssm.New(sess, &aws.Config{})
+	out, err := ssmClient.GetParameter(&ssm.GetParameterInput{
+		Name:           aws.String(key),
+		WithDecryption: aws.Bool(true),
+	})
+	if err != nil {
+		panic(err)
+	}
+	return *out.Parameter.Value
+
 }
