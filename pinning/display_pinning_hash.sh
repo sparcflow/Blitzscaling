b1fdc144b46bea63159ad6a0a4b25cfa45725183
sparc flow <sparc.flow@protonmail.com>
2023-03-12 23:22:00 +0100

    handle all types of keys in cert pinning script

--- pinning/display_pinning_hash.sh
+++ pinning/display_pinning_hash.sh
@@ -3,5 +3,5 @@
 
 openssl s_client -servername $1 -connect $1:443 2>&1 < /dev/null |
 openssl x509 -pubkey -noout |
-openssl rsa -pubin -outform der 2>/dev/null |
+openssl pkey -pubin -outform der 2>/dev/null |
 openssl dgst -sha256 -binary | openssl enc -base64
